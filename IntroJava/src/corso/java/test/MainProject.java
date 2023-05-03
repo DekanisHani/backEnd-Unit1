f0aee0d63b355585f6a05ffe6a83165f6262930f
DekanisHani <dekanis@outlook.it>
2023-05-03 10:11:06 +0200

    giorno1

---
+++ IntroJava/src/corso/java/test/MainProject.java
@@ -0,0 +1,22 @@
+package corso.java.test;
+
+public class MainProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean b = true;
+		char c = 'f';
+		int i = 25;
+		long l = i;
+		double d= 25.3;
+		float f = 25.3f;
+		//String str = new String();
+		String s = "ciao";
+		s = "abc";
+		final String ABC = "Costante";
+		//ABC = "ciaooo"; ERRORE (FINAL NON CONSENTE DI ASSEGNARE NUOVI VALORI)
+		System.out.println("ciao a tutti");
+		
+	}
+
+}
---
+++ IntroJava/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Dekan
+ *
+ */
+module IntroJava {
+}
