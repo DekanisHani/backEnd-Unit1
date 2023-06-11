e49d6ced763819cbe6b2d779631aa01ed30975e4
DekanisHani <dekanis@outlook.it>
2023-06-11 22:57:56 +0200

    Progetto Week5

---
+++ ProgettoWeek5/src/main/java/com/progettoWeek5/epicode/ProgettoWeek5/ProgettoWeek5Application.java
@@ -0,0 +1,13 @@
+package com.progettoWeek5.epicode.ProgettoWeek5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoWeek5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoWeek5Application.class, args);
+	}
+
+}
---
+++ ProgettoWeek5/src/test/java/com/progettoWeek5/epicode/ProgettoWeek5/ProgettoWeek5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progettoWeek5.epicode.ProgettoWeek5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoWeek5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
